e3c2e9d5465a0b3f9e0b92857d26e5b652565b01
DhvaniShah06-Project <24010101257@darshan.ac.in>
2025-01-16 15:54:38 +0530

    started

--- Project.java
+++ Project.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
-public class Project{
-    public static void main(String[] args) {
-        
+public class Project {
+
+    public static void main (String args[]){
+        //railway
     }
 }
